bf7e105fd290f84df4b9bce399b3ecce8406eeb6
rajibrhasan <rajib.rhasan@gmail.com>
2024-11-24 03:26:26 +0600

    Debugging weight certainty

--- client.py
+++ client.py
@@ -38,6 +38,7 @@ class Client(object):
         self.y = y
         self.model.to(self.device)
         outputs = self.model(self.x.to(self.device))
+        print('Outputs: ', outputs)
 
         if self.cfg.MODEL.ADAPTATION == 'roid':
             weights_div = 1 - F.cosine_similarity(self.class_probs_ema.unsqueeze(dim=0), outputs.softmax(1), dim=1)
